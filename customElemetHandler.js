944a9e56cf747c714b4fbbace50ee0787078a21e
Mohd Faraz <androiabledroid@gmail.com>
2025-01-14 20:11:29 +0530

    SilverLine: Import Header from https://github.com/Evavic44/responsive-navbar-with-dropdown"
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- customElemetHandler.js
+++ customElemetHandler.js
@@ -1,4 +1,13 @@
 const templatePath = "/templates/";
+class SpecialHeader extends HTMLElement {
+  async connectedCallback() {
+    const response = await fetch(templatePath + "header.html"); // Path to the external header file
+    const html = await response.text();
+    this.innerHTML = html;
+    // Dispatch a custom event to signal that the header is loaded
+    this.dispatchEvent(new CustomEvent("headerLoaded", { bubbles: true }));
+  }
+}
 class SpecialFooter extends HTMLElement {
   async connectedCallback() {
     const response = await fetch(templatePath + "footer.html"); // Path to the external header file
@@ -8,4 +17,5 @@ class SpecialFooter extends HTMLElement {
 }
 
 // Register the custom element
+customElements.define("special-header", SpecialHeader);
 customElements.define("special-footer", SpecialFooter);
